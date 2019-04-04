af414800af9a67b0892eec6c93cd82ef4cfbc127
SmithLinden <47782988+SmithLinden@users.noreply.github.com>
2019-04-04 19:02:09 +0100

    Update news.py
    has batch normalisation, dropout, batch normalisation and scaled data; still has nan errors though from exploding gradients. Not sure exactly how to fix it: clipping the gradients for the optimiser, in the loss fn, somehow.

--- news.py
+++ news.py
@@ -28,16 +28,20 @@ x_train_tfidf = vec.fit_transform(news_train.data)
 x_test_tfidf = vec.transform(news_test.data)
 
 
+from sklearn.preprocessing import normalize  # Normalising the x train and test
+x_train_tfidf = normalize(x_train_tfidf, norm='l1', axis=1)
+x_test_tfidf = normalize(x_test_tfidf, norm='l1', axis=1)
 
 
 # Parameters
-learning_rate = 0.01
+learning_rate = 0.001
 num_steps = 1
 batch_size = 128
 display_step = 100
+keep_prob = 0.5
 
 # Network Parameters
-n_hidden_1 = 256 # 1st layer number of neurons
+n_hidden_1 = 512 # 1st layer number of neurons
 n_hidden_2 = 256 # 2nd layer number of neurons
 num_input = 35788 # for each word in our dictionary
 num_classes = len(categories)
@@ -45,7 +49,6 @@ num_classes = len(categories)
 # tf Graph input
 X = tf.placeholder("float", [None, num_input])
 Y = tf.placeholder("float", [None, num_classes])
-
 # Store layers weight & bias
 weights = {
     'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
@@ -60,15 +63,44 @@ biases = {
 
 
 # Create model
+# def neural_net(x):
+#     # Hidden fully connected layer with 256 neurons
+#     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+#     # Hidden fully connected layer with 256 neurons
+#     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+#     # Output fully connected layer with a neuron for each class
+#     out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+#
+#     # out_layer = tf.clip_by_value(out_layer, clip_value_min=0.00001, clip_value_max=1)
+#
+#     return out_layer
+
+
+# Create model
 def neural_net(x):
     # Hidden fully connected layer with 256 neurons
+    # We first get weights and biases, then batch normalise, then dropout (or vice versa) and activate last
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    layer_1_b = tf.layers.batch_normalization(layer_1) # batch normalising
+    layer_1_d = tf.nn.dropout(layer_1_b, keep_prob) # dropout
+    layer_1_f = tf.nn.relu(layer_1_d) # activation with relu
+
     # Hidden fully connected layer with 256 neurons
-    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    layer_2 = tf.add(tf.matmul(layer_1_d, weights['h2']), biases['b2'])
+    layer_2_b = tf.layers.batch_normalization(layer_2)
+    layer_2_d = tf.nn.dropout(layer_2_b, keep_prob)
+    layer_2_f = tf.nn.relu(layer_2_d)
+
     # Output fully connected layer with a neuron for each class
-    out_layer = tf.matmul(layer_2, weights['out']) + biases['out']
+    out_layer = tf.nn.relu(tf.matmul(layer_2_d, weights['out']) + biases['out'])
+    out_layer_b = tf.layers.batch_normalization(out_layer)
+    out_layer_d = tf.nn.dropout(out_layer, keep_prob)
+    out_layer_f = tf.nn.relu(out_layer_d)
+
+    # out_layer_f = tf.clip_by_value(out_layer, clip_value_min=0.00001, clip_value_max=1)
 
     return out_layer
+
 
 # Construct model
 logits = neural_net(X)
@@ -78,15 +110,25 @@ prediction = tf.nn.softmax(logits)
 loss_op = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(
     logits=logits, labels=Y))
 
+# loss_op = tf.reduce_sum(tf.nn.sparse_softmax_cross_entropy_with_logits(
+#     logits=logits, labels=Y))
+#
+# loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=logits, labels=Y))
+#
+
+
 # def loss_op_fix(logits, labels):
+#
+#     logits = tf.clip_by_value(logits, clip_value_min=0.00001, clip_value_max=1)
+#
 #     loss_op = tf.reduce_sum(tf.nn.softmax_cross_entropy_with_logits(
 #         logits=logits, labels=labels))
 #
-#     tf.is_nan
-#
-#     for i in loss_op:
-#         if np.isnan(i) is True:
-#             i = 0
+#     # w = tf.where(tf.is_nan(X), tf.ones_like(X) * 0, X);  # if w is nan use 1 * NUMBER else use element in w
+#     #
+#     # for i in loss_op:
+#     #     if np.isnan(i) is True:
+#     #         i = 0
 #     return(loss_op)
 #
 # loss_op = loss_op_fix(logits, Y)
@@ -129,6 +171,7 @@ with tf.Session() as sess:
                 print("Step " + str(step) + ", Minibatch " + str(index) + " Loss= " + \
                       "{:.4f}".format(loss) + ", Training Accuracy= " + \
                       "{:.3f}".format(acc))
+        print("Step Completed...")
 
     print("Optimization Finished!")
 
