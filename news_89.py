fdd214af3b03de63270393b5a7c0fafb238f47d4
SmithLinden <47782988+SmithLinden@users.noreply.github.com>
2019-04-07 17:48:00 +0100

    Update news_89.py

--- news_89.py
+++ news_89.py
@@ -1,16 +1,11 @@
 from sklearn.datasets import fetch_20newsgroups
 import tensorflow as tf
 import numpy as np
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
-from nltk.corpus import stopwords
-import nltk
-import string
 
-nltk.download('punkt')
-nltk.download('stopwords')
 
-news = fetch_20newsgroups(subset='all')
+seed = 12345
+
+news = fetch_20newsgroups(subset='all', random_state=seed)
 
 categories = range(0, 20)
 
@@ -29,50 +24,46 @@ y_val = tf.keras.utils.to_categorical(y_val, len(categories))
 
 
 
+
 from sklearn.feature_extraction.text import TfidfVectorizer
-# vec = TfidfVectorizer(binary=False, strip_accents='ascii', use_idf=True,
-#                       max_features=12500, stop_words='english')   # Turning to term frequency inverse doc freq
 vec = TfidfVectorizer(binary=False, use_idf=True,
-                      max_features=12500, stop_words='english')
+                      max_features=12500, stop_words='english')  # term freq invers doc freq transform
 x_train_tfidf = vec.fit_transform(x_train)
 x_test_tfidf = vec.transform(x_test)
 x_val_tfidf = vec.transform(x_val)
 
-from sklearn.preprocessing import normalize  # Normalising the x train and test, seems to reduce performance
+from sklearn.preprocessing import normalize  # Normalising the x train,test and val
 x_train_tfidf = normalize(x_train_tfidf, norm='l2', axis=1)
 x_test_tfidf = normalize(x_test_tfidf, norm='l2', axis=1)
 x_val_tfidf = normalize(x_val_tfidf, norm='l2', axis=1)
 
 
 
-# Parameters
+# hyper-parameters
 learning_rate = 0.001
 num_steps = 16
 batch_size = 64
 display_step = 1
 keep_prob = 0.5
+seed = 23
+
+tf.set_random_seed(seed)
+np.random.seed(seed)
+
 
 # Network Parameters
-n_hidden_1 = 512  # 1st layer number of neurons
-n_hidden_2 = 512  # 2nd layer number of neurons
+n_hidden_1 = 512  # hidden 1 neurons
+n_hidden_2 = 512  # hidden 2 neurons
 n_hidden_3 = 256
 num_input = x_train_tfidf.shape[1]  # for each word in our dictionary
 num_classes = len(categories)  # each category (20 in full model)
 
-# tf Graph input
+# tf inputs x and labels y
 X = tf.placeholder("float", [None, num_input])  # x params stored here; each x is a tfidf for a news instance
 Y = tf.placeholder("float", [None, num_classes])  # labels here
 
-# weights and biases for each layer
-# weights = {
-#     'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
-#     'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
-#     'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
-#     'out': tf.Variable(tf.random_normal([n_hidden_1, num_classes]))
-# }
-
 initialiser = tf.contrib.layers.xavier_initializer()
-
+zero_init = tf.zeros_initializer()
 weights = {
     'h1': tf.Variable(initialiser([num_input, n_hidden_1])),
     'h2': tf.Variable(initialiser([n_hidden_1, n_hidden_2])),
@@ -81,22 +72,23 @@ weights = {
 }
 
 biases = {
-    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
-    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
-    'b3': tf.Variable(tf.random_normal([n_hidden_3])),
-    'out': tf.Variable(tf.random_normal([num_classes]))
+    'b1': tf.Variable(zero_init([n_hidden_1])),
+    'b2': tf.Variable(zero_init([n_hidden_2])),
+    'b3': tf.Variable(zero_init([n_hidden_3])),
+    'out': tf.Variable(zero_init([num_classes]))
 }
 
 
 def neural_net(x):
-    # Hidden fully connected layer with n neurons
+    """ this is the network itself -- note that the softmax activation is outside this fn"""
+
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
     layer_1 = tf.nn.relu(layer_1)
     layer_1 = tf.layers.batch_normalization(layer_1)  # batch normalising
 
     layer_1 = tf.layers.dropout(layer_1, keep_prob)
 
-    # Hidden fully connected layer
+
     layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
 
     layer_2 = tf.nn.relu(layer_2)
@@ -113,28 +105,28 @@ def neural_net(x):
     return out_layer
 
 
-# Construct model
+# logits, building model...
 logits = neural_net(X)
 prediction = tf.nn.softmax(logits)
 
-# Define loss and optimizer
+# Loss is softmax crossentropy, optimiser adam
 loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(
     logits=logits, labels=Y))
 optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
 train_op = optimizer.minimize(loss_op)
 
-# Evaluate model
+# get predictions and compare accuracy
 correct_pred = tf.equal(tf.argmax(prediction, 1), tf.argmax(Y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
-# Initialize the variables (i.e. assign their default value)
+## initialise and assign weight/bias initialisations
 init = tf.global_variables_initializer()
 
 with tf.Session() as sess:
     # Run the initializer
     sess.run(init)
 
-    # sess = tf_debug.LocalCLIDebugWrapperSession(sess)
+    # sess = tf_debug.LocalCLIDebugWrapperSession(sess) # for debugging
     # sess.add_tensor_filter("has_inf_or_nan", tf_debug.has_inf_or_nan)
 
     train_acc = list()
@@ -150,7 +142,7 @@ with tf.Session() as sess:
 
             batch_y = y_train[offset: offset + batch_size, ]
 
-            # Run optimization op (backprop)
+            # backpropagate
             sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
 
             if step % display_step == 0 or step == 1 or True:
@@ -164,7 +156,7 @@ with tf.Session() as sess:
         print("Step Completed..." + "Mean Accuracy of Last Epoch:" + \
               str(np.mean(train_acc[-batch_size:])))
 
-        """ if validation accuracy some % off the train, do not go to next epoch"""
+        """ if validation accuracy decrease, do not go to next epoch"""
         """ also save checkpoints here... """
 
 
@@ -194,7 +186,7 @@ with tf.Session() as sess:
             print("No Validation Improvement")
             break
 
-        print("Would you like to continue training?....")
+        print("Would you like to continue training?.... 10 seconds to default continue...")
         answer = input('y/n')
         if answer == 'n':
             break
@@ -212,10 +204,8 @@ with tf.Session() as sess:
 
         batch_y = y_test[offset: offset + batch_size, ]
 
-        # Run optimization -- this continues training with the test set, which maybe we shouldn't do?
-        # sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
 
-        # Calculate batch loss and accuracy
+        # loss and accuracy -- no longer running the training operation, not changing weights
         loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x,
                                                              Y: batch_y})
         print("Minibatch " + str(index) + " Loss= " + \
@@ -229,4 +219,4 @@ with tf.Session() as sess:
     saver = tf.train.Saver()
 
     save_path = saver.save(sess, "/tmp/model.ckpt")
-    print("SAVED MODEL TO PATH: %s" % save_path)
+    print("SAVED MODEL TO: %s" % save_path)
